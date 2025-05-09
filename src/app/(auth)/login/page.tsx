b118fd78fc66a4b0acb60d3751190ebbfd010ab4
hakenshi <fkdias025@gmail.com>
2025-05-09 13:47:58 -0300

    deleted default page.tsx file, created /login and /home routes.

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+    return (
+        <main className="bg-red-600 h-screen w-screen">
+            <section>
+                <h1 className="text-red-600">XD</h1>
+            </section>
+        </main>
+  )
+}
